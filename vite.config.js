28cbfdb88b218e4831381e397eb587cf987851af
Hodong <fire13764@gmail.com>
2023-03-27 19:56:53 +0900

    firebase hosting 연결

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react_shopping_mall/',
   build: { chunkSizeWarningLimit: 1600, }
 })
